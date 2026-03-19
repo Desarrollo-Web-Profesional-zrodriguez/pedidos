d801b10f587cbccc7dde2e8c35d5d90e93a7262f
ZahirRodriguez <rodriguez.mora.zahir.15@gmail.com>
2026-03-18 23:14:54 -0600

    task: deploy in railway

--- frontend_pedidos/src/servicios/api.js
+++ frontend_pedidos/src/servicios/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: import.meta.env.PROD
-        ? "https://amiable-nourishment-production-76ea.up.railway.app/api/v1"
-        : "http://localhost:3001/api/v1"
+    baseURL: "https://amiable-nourishment-production-76ea.up.railway.app/api/v1"
+    // baseURL: "http://localhost:3001/api/v1"
 });
 
 export default API;
